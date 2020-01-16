0255bd72cc66bb755401b58881e4ac29b2b84872
Michal Charvát <20499772+mi-char@users.noreply.github.com>
2020-01-16 17:11:13 +0100

    fix: Fixed behavior of json decoders to return DecodingFailure (#1)
    fix: Fixed behavior of json Circe decoders to return DecodingFailure and not throw exception in case of failure. Added tests for Circe decoders.

---
+++ src/test/scala/com/avast/scala/hashes/circe/Sha1DecoderTest.scala
@@ -0,0 +1,27 @@
+package com.avast.scala.hashes.circe
+
+import com.avast.scala.hashes.Sha1
+import io.circe.{DecodingFailure, Json}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
+
+@RunWith(classOf[JUnitRunner])
+class Sha1DecoderTest extends FlatSpec with Matchers {
+  "it" should "deserialize valid Sha1" in {
+    val value = Sha1("0fd08a268f6032ce2a83a17ac8adceaf82ade5e3")
+
+    val result = Sha1Decoder.decodeJson(Json.fromString(value.toString))
+
+    assertResult(Right(value))(result)
+  }
+
+  "deserialization" should "correctly fail in case of invalid Sha1" in {
+    val result = Sha1Decoder.decodeJson(Json.fromString("InvalidValue"))
+
+    result match {
+      case Left(_: DecodingFailure) => succeed
+      case _ => fail()
+    }
+  }
+}
