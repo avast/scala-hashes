023f2237cfd470e9461f66d5803ea63eeabd72f3
Charvát Michal <charvat@avast.com>
2020-01-16 15:27:44 +0100

    fix: Fixed behaviour of json decoders to return DecodingFailure and not throw exception in case of failure.

--- src/main/scala/com/avast/scala/hashes/circe/package.scala
+++ src/main/scala/com/avast/scala/hashes/circe/package.scala
@@ -1,12 +1,31 @@
 package com.avast.scala.hashes
 
-import io.circe.{Decoder, Encoder}
+import io.circe._
+
+import scala.reflect.ClassTag
+import scala.util.control.NonFatal
 
 package object circe {
-  implicit val Sha256Decoder = Decoder.decodeString.map(Sha256(_))
-  implicit val Sha256Encoder = Encoder.encodeString.contramap((s: Sha256) => s.toString)
-  implicit val MD5Decoder = Decoder.decodeString.map(MD5(_))
-  implicit val MD5Encoder = Encoder.encodeString.contramap((h: MD5) => h.toString)
-  implicit val Sha1Decoder = Decoder.decodeString.map(Sha1(_))
-  implicit val Sha1Encoder = Encoder.encodeString.contramap((s: Sha1) => s.toString)
+  implicit val Sha256Decoder: Decoder[Sha256] = prepareDecoder(Sha256(_))
+  implicit val Sha256Encoder: Encoder[Sha256] = Encoder.encodeString.contramap((s: Sha256) => s.toString)
+  implicit val MD5Decoder: Decoder[MD5] = prepareDecoder(MD5(_))
+  implicit val MD5Encoder: Encoder[MD5] = Encoder.encodeString.contramap((h: MD5) => h.toString)
+  implicit val Sha1Decoder: Decoder[Sha1] = prepareDecoder(Sha1(_))
+  implicit val Sha1Encoder: Encoder[Sha1] = Encoder.encodeString.contramap((s: Sha1) => s.toString)
+
+  private def prepareDecoder[A: ClassTag](parse: String => A): Decoder[A] =
+    (c: HCursor) => {
+      c.value.as[String].flatMap { s =>
+        try {
+          Right(parse(s))
+        } catch {
+          case NonFatal(_) =>
+            Left {
+              DecodingFailure(
+                implicitly[ClassTag[A]].runtimeClass.getSimpleName,
+                c.history)
+            }
+        }
+      }
+    }
 }
