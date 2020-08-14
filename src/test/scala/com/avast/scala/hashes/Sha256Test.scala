7fc00736f91afe47166d570aba43f159712c0eae
Michal Augustýn <augustyn@avast.com>
2020-08-14 14:48:55 +0200

    chore: test that proves base64 has still the same length

--- src/test/scala/com/avast/scala/hashes/Sha256Test.scala
+++ src/test/scala/com/avast/scala/hashes/Sha256Test.scala
@@ -18,4 +18,8 @@ class Sha256Test extends AnyFlatSpec with Matchers {
   it should "convert the Sha256 to lower-case string" in {
     Sha256("6A18B3C45107538DE9D430F83A6AF988EDBDDEB4E5A6BDB16F223A2FA37EE446").toString() shouldBe "6a18b3c45107538de9d430f83a6af988edbddeb4e5a6bdb16f223a2fa37ee446"
   }
+
+  it should "generate base64 of the same length for random SHA256" in {
+     (1 to 32).map(_ => Sha256(HashesMother.randomHexString(64)).toBase64String.length) shouldBe (1 to 32).map(_ => 44)
+  }
 }
