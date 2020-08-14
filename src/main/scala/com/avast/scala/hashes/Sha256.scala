6da7717c01c79ad909fa83e21936d63b0e3f96b1
Michal Augustýn <augustyn@avast.com>
2020-08-14 14:28:12 +0200

    feat: toHexString added to allow to be specific where required

--- src/main/scala/com/avast/scala/hashes/Sha256.scala
+++ src/main/scala/com/avast/scala/hashes/Sha256.scala
@@ -6,6 +6,7 @@ case class Sha256(bytes: Array[Byte]) {
   require(bytes.length == 32, s"Invalid Sha256: ${Sha256.bytesLength} bytes expected but ${bytes.length} provided")
 
   final def toBase64String: String = bytes2base64(bytes)
+  final def toHexString: String = bytes2hex(bytes)
 
   override def toString: String = bytes2hex(bytes)
   override def hashCode(): Int = util.Arrays.hashCode(bytes)
